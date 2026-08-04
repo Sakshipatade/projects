c8fcd4610d06d4abd45481eec319432a4b4aa9bb
Sakshipatade <sakshivpatade@gmail.com>
2026-08-04 15:39:24 +0530

    use of context manager

--- client_server_arch/client.py
+++ client_server_arch/client.py
@@ -7,5 +7,27 @@ while True:
     my_msg = input('Ask: ')
     client_socket.send(my_msg.encode())
 
-    response = client_socket.recv(1024).decode()
-    print('Server: ', response)
+    # response = client_socket.recv(1024).decode()
+    # print('Server: ', response)
+    
+    if my_msg.lower() == 'exit':
+        print('closing client connection.')
+        break
+
+
+client_socket.close()
+
+
+# We can write it using the context managers so we don't need to close the connection manually
+import socket
+
+with socket.socket() as client_socket:
+    client_socket.connect(('localhost', 8080))
+
+    while True:
+        msg = input('ask: ')
+        client_socket.send(msg.encode())
+
+        if msg.strip().lower() == 'exit':
+            print('connection closed..')
+            break
--- client_server_arch/server.py
+++ client_server_arch/server.py
@@ -3,6 +3,7 @@ import socket
 
 # create a socket
 server_socket = socket.socket()
+# Telling client that use 3000 to connect with me..
 server_socket.bind(('localhost',3000))
 server_socket.listen(1)
 
@@ -17,7 +18,28 @@ while True:
     if not data:
         break
     print('client: ', data)
-    conn.send(f'Received: {data}'.encode())
+    # conn.send(f'Received: {data}'.encode())
 
 conn.close()
 server_socket.close()
+
+
+# We can write it using the context managers so we don't need to close the connection manually
+
+import socket
+
+with socket.socket() as server_socket:
+    server_socket.bind(('localhost', 8080))
+    server_socket.listen()
+
+    print('I am ready and listeing on port 8080')
+
+    conn, addr = server_socket.accept()
+    print(f'Client address: {addr}')
+
+    while True:
+        data = conn.recv(1024).decode()
+        if not data:
+            print('connection closed')
+            break
+        print(f'Client data: {data}')
